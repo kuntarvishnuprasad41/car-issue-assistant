ba16c3cf6f80da8645b6747a17b0ea6fb9a676ee
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2025-01-14 20:57:37 +0530

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,15 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-white ">
       <div className="z-10 w-full max-w-5xl items-center text-center justify-between font-mono text-sm">
-        <h1 className="text-4xl font-bold mb-8 text-center">Test Cars</h1>
-        {carData.cars.join(", ")}
-        <br />
+        <h1 className="text-4xl font-bold mb-8 text-center">
+          Test Cars are below chat
+        </h1>
         Spelling mistakes will be accepted too
+        <br />
+        <br />
         <CarIssueAssistant />
+        <br />
+        {carData.cars.join(", ")}
       </div>
     </main>
   );
