e7d5063199f00a4fe3708653905b62ac8db300b2
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2025-01-23 10:24:39 +0530

    Chatbot Fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,9 @@ import carData from "../app/data/carData.json";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between xl:p-24 ">
-      <div className="z-10 w-full  items-center text-center justify-between font-mono text-sm">
-        <h1 className="text-4xl font-bold mb-8 text-center">
-          Test Cars are below chat
-        </h1>
-        Spelling mistakes will be accepted too
-        <br />
-        <br />
+    <main className="flex min-h-screen flex-col items-center justify-between xl:p-24 bg-black ">
+      <div className="z-10 w-full  items-center text-center justify-between font-mono text-sm bg-black">
+        
         <CarIssueAssistant />
         <br />
         {carData.cars.join(", ")}
